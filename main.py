a4a23eaf8d8ea46bff8bcd18fa2bde736f495f20
Patrick Clary <patrickm@theclarys.org>
2025-06-05 12:19:08 -0500

    Add asteroid and asteroidfield classes

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 #main function
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}") 
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-    
+    asteroid_field = AsteroidField()
+
     game_clock = pygame.time.Clock()
     dt = 0
 
